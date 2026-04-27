818d870659bdaf337a5e917a29a0cfc6a0354dca
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-04-27 00:35:11 -0600

    Arreglo controller

--- Controller.py
+++ Controller.py
@@ -1,39 +1,12 @@
 import random
-import Nodo
-
-class BST:
+import BST
+import SplayTree
+import RedBlakc
+class Controller:
     def __init__(self):
-        self.root = None
- 
-    def insert(self, key):
-        nuevo = Nodo(key)
-        if self.root is None:
-            self.root = nuevo
-            return
-        actual = self.root
-        while True:
-            if key < actual.data:
-                if actual.left is None:
-                    actual.left = nuevo
-                    nuevo.parent = actual
-                    return
-                actual = actual.left
-            else:
-                if actual.right is None:
-                    actual.right = nuevo
-                    nuevo.parent = actual
-                    return
-                actual = actual.right
- 
-    def find(self, key):
-        actual = self.root
-        iteraciones = 0
-        while actual is not None:
-            iteraciones += 1
-            if key == actual.data:
-                return actual, iteraciones
-            elif key < actual.data:
-                actual = actual.left
-            else:
-                actual = actual.right
-        return None, iteraciones
+        self.arboles = {
+            "BST": BST(),
+            "SplayTree": SplayTree(),
+            "RedBlackTree": RedBlakc()
+        }
+        self.resultados = {}
