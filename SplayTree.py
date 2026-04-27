c15ba17fe2041088da8bfb447df4d1b27f814a93
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-04-27 00:37:17 -0600

    Insercion splay

--- SplayTree.py
+++ SplayTree.py
@@ -2,6 +2,27 @@ class SplayTree:
     def __init__(self):
         self.root = None
 
+    def insert(self, key):
+        from Nodo import Nodo
+        nuevo = Nodo(key)
+        if self.root is None:
+            self.root = nuevo
+            return
+        actual = self.root
+        padre = None
+        while actual is not None:
+            padre = actual
+            if key < actual.data:
+                actual = actual.left
+            else:
+                actual = actual.right
+        nuevo.parent = padre
+        if key < padre.data:
+            padre.left = nuevo
+        else:
+            padre.right = nuevo
+        self._splay(nuevo)
+
     def find(self, key):
         actual = self.root
         iteraciones = 0
