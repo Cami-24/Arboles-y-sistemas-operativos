eb02be5225099206f115855ee75c143567893ac8
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-04-27 00:38:22 -0600

    Splay procesos

--- SplayTree.py
+++ SplayTree.py
@@ -2,6 +2,58 @@ class SplayTree:
     def __init__(self):
         self.root = None
 
+     def _right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+ 
+    def _left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+ 
+    def _splay(self, x):
+        while x.parent is not None:
+            p = x.parent
+            g = p.parent
+            if g is None:                          # zig
+                if x == p.left:
+                    self._right_rotate(p)
+                else:
+                    self._left_rotate(p)
+            elif x == p.left and p == g.left:      # zig-zig
+                self._right_rotate(g)
+                self._right_rotate(p)
+            elif x == p.right and p == g.right:    # zig-zig espejo
+                self._left_rotate(g)
+                self._left_rotate(p)
+            elif x == p.right and p == g.left:     # zig-zag
+                self._left_rotate(p)
+                self._right_rotate(g)
+            else:                                   # zig-zag espejo
+                self._right_rotate(p)
+                self._left_rotate(g)
+
     def insert(self, key):
         from Nodo import Nodo
         nuevo = Nodo(key)
